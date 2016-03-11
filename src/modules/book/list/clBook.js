02a6a91b2c222799e20679306face2844542e20f
Ian Corne <ian.corne.be@gmail.com>
2016-03-11 10:09:14 +0100

    make dumb compomentn

--- src/modules/book/list/clBook.js
+++ src/modules/book/list/clBook.js
@@ -1,18 +1,3 @@
-import angular from 'angular';
-import booksTemplate from './book.html';
-import BookController from './booklistcontroller';
-import BookService from './../services/bookservice';
-
-let module = angular.module('book.list', [BookService])
-    .directive('clBook', BookListDirective);
-
-function BookListDirective() {
-    return {
-        restrict: 'E',
-        template: booksTemplate,
-        controller: BookController,
-        controllerAs: 'model',
-        scope: {}
-    }
-}
-export default module.name;
+/**
+ * Created by icorne on 10.03.16.
+ */
